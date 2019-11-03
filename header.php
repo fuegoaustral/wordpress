ee470bef2a0740ff783d47424ce54c3240bfdb2b
Federico Bond <federicobond@gmail.com>
2019-11-03 17:51:48 -0300

    Add favicon

--- header.php
+++ header.php
@@ -7,6 +7,7 @@
 	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
 	<link href="https://fonts.googleapis.com/css?family=Amatic+SC|Open+Sans:400,600&display=swap" rel="stylesheet">
 	<link href="+SC|Open+Sans&display=swap" rel="stylesheet">
+	<link rel="icon" href="<?php echo get_template_directory_uri(); ?>/img/favicon4.png" type="image/png">
 
 	<?php wp_head(); ?>
 </head>
