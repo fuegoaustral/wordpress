eaf9d1fdc8335e988c5716493b8d40c9f11d6ce7
Federico Bond <federicobond@gmail.com>
2019-11-03 15:13:30 -0300

    Add home.php template

---
+++ home.php
@@ -0,0 +1,12 @@
+<?php
+
+get_header();
+?>
+
+	<div id="primary" class="content-area">
+		<main id="main" class="site-main">
+		</main><!-- #main -->
+	</div><!-- #primary -->
+
+<?php
+get_footer();
