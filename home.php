d3b157e61e0ccf50a4b828cedcd8913f758df2f0
Federico Bond <federicobond@gmail.com>
2019-11-03 18:55:55 -0300

    Continue work on the home page

--- home.php
+++ home.php
@@ -38,10 +38,27 @@
 			</div>
 	</div>
 
-	<div id="primary" class="content-area">
-		<main id="main" class="site-main">
-		</main><!-- #main -->
-	</div><!-- #primary -->
+	<div class="current-edition">
+		<div class="container">
+			<div class="row justify-content-center">
+				<div class="col-9">
+					<div class="card">
+						<div class="card-body">
+							<h2>Sincronismo</h2>
+							<h3>1 al 5 de marzo de 2019</h3>
+							<p>Fuego Austral nos convoca una vez más. Nuestra ciudad, esa a la que siempre pertenecimos, esa que elegimos, vuelve a levantarse desde la tierra para abrazarnos en su espiral. En vísperas de carnaval coincidimos en un encuentro de almas que fluyen al ritmo de su propio latido.</p>
+
+							<p>El sincronismo de ser nuestra ciudad áurea nos hermana en un nautilus de vibraciones en el que nos damos la oportunidad de ser nuestra versión más elevada. No importa por qué venimos ni de dónde. Cada uno de los pasos que dimos en el universo y nos trajeron al encuentro se transforman ahora en el ritmo de nuestro espiral, donde sincronizados en la eterna inmediatez de Fuego Austral, estamos finalmente juntos otra vez. Bienvenidos a casa.</p>
+						</div>
+						<div class="card-body card-actions">
+							<a class="btn btn-primary" href="#">Comprar entradas</a>
+							<a class="btn btn-default ml-2" href="#">Más información</a>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div><!-- .current-edition -->
 
 <?php
 get_footer();
