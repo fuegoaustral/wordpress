cd3c03f830704fde4b805bdb894e2438057cc83b
Federico Bond <federicobond@gmail.com>
2019-11-04 11:37:49 -0300

    Fix content widths in home

--- home.php
+++ home.php
@@ -40,81 +40,73 @@
 
 <div class="current-edition">
 	<div class="container">
-		<div class="row justify-content-center">
-			<div class="col-10">
-				<div class="card">
-					<div class="card-body">
-						<div class="row">
-							<div class="col-7">
-								<h2>Satori</h2>
-								<h3>21 al 26 de febrero de 2020</h3>
-								<p>
-									Hogar... dulce y alucinante hogar.
-									<br>
-									Volver a ese lugar que se descubre de forma clara
-									<br>
-									que solo existe el presente,
-									<br>
-									allí donde nace el pasado y el futuro nos complace,
-									<br>
-									creándose y disolviéndose en el mismo instante.
-								</p>
-								<p>
-									Ese Satori en donde la experiencia ilumina tu presente
-									<br>
-									y el tiempo es sólo tardanza de lo que está por venir
-									<br>
-									que el pasado es un invento y el futuro una alucinación.
-								</p>
-								<p>
-									En donde el fuego está eufórico por calmar la razón,
-									<br>
-									mientras la pampa es un destello
-									<br>
-									repentino de movimientos constantes,
-									<br>
-									con movimientos creativos, que suceden,
-									<br>
-									en esa casa que existe
-									<br>
-									gracias a cada uno de sus mágicos y amorosos habitantes.
-								</p>
-							</div>
-							<div class="col-5">
-								<img src="<?php echo get_template_directory_uri(); ?>/img/satori.png" alt="Satori: Fuego Austral 2020" width="100%">
-							</div>
-						</div>
+		<div class="card">
+			<div class="card-body">
+				<div class="row">
+					<div class="col-7">
+						<h2>Satori</h2>
+						<h3>21 al 26 de febrero de 2020</h3>
+						<p>
+							Hogar... dulce y alucinante hogar.
+							<br>
+							Volver a ese lugar que se descubre de forma clara
+							<br>
+							que solo existe el presente,
+							<br>
+							allí donde nace el pasado y el futuro nos complace,
+							<br>
+							creándose y disolviéndose en el mismo instante.
+						</p>
+						<p>
+							Ese Satori en donde la experiencia ilumina tu presente
+							<br>
+							y el tiempo es sólo tardanza de lo que está por venir
+							<br>
+							que el pasado es un invento y el futuro una alucinación.
+						</p>
+						<p>
+							En donde el fuego está eufórico por calmar la razón,
+							<br>
+							mientras la pampa es un destello
+							<br>
+							repentino de movimientos constantes,
+							<br>
+							con movimientos creativos, que suceden,
+							<br>
+							en esa casa que existe
+							<br>
+							gracias a cada uno de sus mágicos y amorosos habitantes.
+						</p>
 					</div>
-					<div class="card-body card-actions">
-						<a class="btn btn-primary" href="#">Comprar entradas</a>
-						<a class="btn btn-default ml-2" href="#">Más información</a>
+					<div class="col-5">
+						<img src="<?php echo get_template_directory_uri(); ?>/img/satori.png" alt="Satori: Fuego Austral 2020" width="100%">
 					</div>
 				</div>
+			</div>
+			<div class="card-body card-actions">
+				<a class="btn btn-primary" href="#">Comprar entradas</a>
+				<a class="btn btn-default ml-2" href="#">Más información</a>
 			</div>
 		</div>
 	</div>
 
 	<div class="container no-spectators">
-		<div class="row justify-content-center">
-			<div class="col-10">
-				<h2>Espectadores, ¡no!</h2>
+		<h2>Espectadores, ¡no!</h2>
 
-				<p class="lead">A Fuego Austral lo construímos entre todos, todo lo que sucede en Fuego Austral está hecho por gente como vos, por nosotros, desde el proceso de creación hasta el armado de infraestructuras, campamentos, obras de arte y luego el desarmado de la ciudad para que todo quede como si no hubiésemos estado ahí (no dejar rastro). Todos podemos ser voluntarios y cuantos más seamos mejor va a ser nuestra ciudad temporal.</p>
-		
-				<div class="row pt-5">
-					<div class="col">
-						<h3>Voluntaries</h3>
-						<p>Por eso, si querés formar parte y ayudar en esta gran tarea pero no sabes cómo, a continuación podés ver las distintas áreas de voluntariado y ver con cuál sentís que podes dar tu mejor vos de acuerdo con tus habilidades, intereses y honesta disponibilidad.</p>
-					</div>
-					<div class="col">
-						<h3>Campamentos</h3>
-						<p>Por eso, si querés formar parte y ayudar en esta gran tarea pero no sabes cómo, a continuación podés ver las distintas áreas de voluntariado y ver con cuál sentís que podes dar tu mejor vos de acuerdo con tus habilidades, intereses y honesta disponibilidad.</p>
-					</div>
-					<div class="col">
-						<h3>Arte</h3>
-						<p>Por eso, si querés formar parte y ayudar en esta gran tarea pero no sabes cómo, a continuación podés ver las distintas áreas de voluntariado y ver con cuál sentís que podes dar tu mejor vos de acuerdo con tus habilidades, intereses y honesta disponibilidad.</p>
-					</div>
-				</div>
+		<p class="lead">A Fuego Austral lo construímos entre todos, todo lo que sucede en Fuego Austral está hecho por gente como vos, por nosotros, desde el proceso de creación hasta el armado de infraestructuras, campamentos, obras de arte y luego el desarmado de la ciudad para que todo quede como si no hubiésemos estado ahí (no dejar rastro). Todos podemos ser voluntarios y cuantos más seamos mejor va a ser nuestra ciudad temporal.</p>
+
+		<div class="row pt-5">
+			<div class="col">
+				<h3>Voluntaries</h3>
+				<p>Por eso, si querés formar parte y ayudar en esta gran tarea pero no sabes cómo, a continuación podés ver las distintas áreas de voluntariado y ver con cuál sentís que podes dar tu mejor vos de acuerdo con tus habilidades, intereses y honesta disponibilidad.</p>
+			</div>
+			<div class="col">
+				<h3>Campamentos</h3>
+				<p>Por eso, si querés formar parte y ayudar en esta gran tarea pero no sabes cómo, a continuación podés ver las distintas áreas de voluntariado y ver con cuál sentís que podes dar tu mejor vos de acuerdo con tus habilidades, intereses y honesta disponibilidad.</p>
+			</div>
+			<div class="col">
+				<h3>Arte</h3>
+				<p>Por eso, si querés formar parte y ayudar en esta gran tarea pero no sabes cómo, a continuación podés ver las distintas áreas de voluntariado y ver con cuál sentís que podes dar tu mejor vos de acuerdo con tus habilidades, intereses y honesta disponibilidad.</p>
 			</div>
 		</div>
 	</div>
