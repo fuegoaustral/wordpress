0815b2497dc7fcebc97127b946404233d0ffe08d
Federico Bond <federicobond@gmail.com>
2019-11-08 18:32:45 -0300

    Fix search results page

--- search.php
+++ search.php
@@ -10,6 +10,7 @@
 get_header();
 ?>
 
+<div class="container">
 	<section id="primary" class="content-area">
 		<main id="main" class="site-main">
 
@@ -49,6 +50,7 @@ get_header();
 
 		</main><!-- #main -->
 	</section><!-- #primary -->
+</div>
 
 <?php
 get_sidebar();
