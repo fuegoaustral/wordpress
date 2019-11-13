0b7769e8a052312b7b69790ab0ebb3c329b65118
Federico Bond <federicobond@gmail.com>
2019-11-13 11:51:54 -0300

    Add schema.org attributes to html tag

--- header.php
+++ header.php
@@ -1,5 +1,5 @@
 <!doctype html>
-<html <?php language_attributes(); ?>>
+<html itemscope itemtype="http://schema.org/WebPage" <?php language_attributes(); ?>>
 <head>
 	<meta charset="<?php bloginfo( 'charset' ); ?>">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no">
